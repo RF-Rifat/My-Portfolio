18c29571f9cb0f92a5cbad6c25268377ef69624d
RF-Rifat <rfrifat6344@gmail.com>
2024-02-10 10:44:48 +0600

    social icons color set up complete

--- components/Shared/SocialIcons.js
+++ components/Shared/SocialIcons.js
@@ -1,62 +1,47 @@
 import React from "react";
 import "./icon.css";
+import { FaDiscord, FaFacebookSquare, FaGithub, FaLinkedin } from "react-icons/fa";
+import { IoMailOpenOutline } from "react-icons/io5";
+import { FaSquareWhatsapp } from "react-icons/fa6";
 
 const SocialIcons = () => {
   return (
     <>
       <div id="socialicons">
-        <div className="socialicon">
-          <div className="dribbblecolor socialiconcircle1" />
-          <div className="socialiconcircle2">
-            <i className="fa icons fa-dribbble dribbble" />
-          </div>
-        </div>
-        <div className="socialicon">
+        <a href="" className="socialicon">
           <div className="facebookcolor socialiconcircle1" />
-          <div className="socialiconcircle2">
-            <i className="fa icons fa-facebook-square facebook" />
+          <div className="socialiconcircle2 grid place-items-center">
+            <FaFacebookSquare className="facebook h-8 w-8" />
           </div>
-        </div>
-        <div className="socialicon">
-          <div className="googlecolor socialiconcircle1" />
-          <div className="socialiconcircle2">
-            <i className="fa icons fa-google-plus-square google" />
-          </div>
-        </div>
-        <div className="socialicon">
-          <div className="flickrcolor socialiconcircle1" />
-          <div className="socialiconcircle2">
-            <i className="fa icons fa-flickr flickr" />
-          </div>
-        </div>
-        <div className="socialicon">
-          <div className="pinterestcolor socialiconcircle1" />
-          <div className="socialiconcircle2">
-            <i className="fa icons fa-pinterest-square pinterest" />
-          </div>
-        </div>
-        <div className="socialicon">
+        </a>
+        <a href="" className="socialicon">
           <div className="githubcolor socialiconcircle1" />
-          <div className="socialiconcircle2">
-            <i className="fa icons fa-github-alt github" />
+          <div className="socialiconcircle2 grid place-items-center">
+            <FaGithub className="github h-8 w-8" />
           </div>
-        </div>
-        <div className="socialicon">
+        </a>
+        <a className="socialicon">
           <div className="linkedincolor socialiconcircle1" />
-          <div className="socialiconcircle2">
-            <i className="fa icons fa-linkedin-square linkedin" />
+          <div className="socialiconcircle2 grid place-items-center">
+            <FaLinkedin className="h-8 w-8 linkedin" />
           </div>
-        </div>
-        <div className="socialicon">
-          <div className="youtubecolor socialiconcircle1" />
-          <div className="socialiconcircle2">
-            <i className="fa icons fa-youtube-square youtube" />
+        </a>
+        <a className="socialicon">
+          <div className="googlecolor socialiconcircle1"></div>
+          <div className="socialiconcircle2 grid place-items-center">
+            <IoMailOpenOutline className="h-8 w-8 google" />
           </div>
-        </div>
+        </a>
+        <a className="socialicon">
+          <div className="whatsAppColor socialiconcircle1" />
+          <div className="socialiconcircle2 grid place-items-center">
+            <FaSquareWhatsapp className="h-8 w-8 whatsApp" />
+          </div>
+        </a>
         <div className="socialicon">
-          <div className="tumblrcolor socialiconcircle1" />
-          <div className="socialiconcircle2">
-            <i className="fa icons fa-tumblr-square tumblr" />
+          <div className="discordColor socialiconcircle1" />
+          <div className="socialiconcircle2 grid place-items-center">
+            <FaDiscord className="h-8 w-8 discord" />
           </div>
         </div>
       </div>
