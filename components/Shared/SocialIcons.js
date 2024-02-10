21db64ee94d8b0e0a7a5e6b7a8edab901d34389e
RF-Rifat <rfrifat6344@gmail.com>
2024-02-10 11:24:52 +0600

    social icons color set up complete

--- components/Shared/SocialIcons.js
+++ components/Shared/SocialIcons.js
@@ -1,49 +1,55 @@
 import React from "react";
 import "./icon.css";
-import { FaDiscord, FaFacebookSquare, FaGithub, FaLinkedin } from "react-icons/fa";
+import { FaDiscord, FaFacebookSquare, FaGithub, FaLinkedin, FaSkype } from "react-icons/fa";
 import { IoMailOpenOutline } from "react-icons/io5";
 import { FaSquareWhatsapp } from "react-icons/fa6";
 
 const SocialIcons = () => {
   return (
     <>
-      <div id="socialicons">
-        <a href="" className="socialicon">
+      <div className="grid grid-cols-3 place-items-center pl-2 pr-6">
+        <a href="https://www.facebook.com/rfrifat6344" className="socialicon">
           <div className="facebookcolor socialiconcircle1" />
           <div className="socialiconcircle2 grid place-items-center">
             <FaFacebookSquare className="facebook h-8 w-8" />
           </div>
         </a>
-        <a href="" className="socialicon">
+        <a href="https://github.com/RF-Rifat" className="socialicon">
           <div className="githubcolor socialiconcircle1" />
           <div className="socialiconcircle2 grid place-items-center">
             <FaGithub className="github h-8 w-8" />
           </div>
         </a>
-        <a className="socialicon">
+        <a
+          href="https://www.linkedin.com/in/md-rifadul-islam-rifad-90aa612a5"
+          className="socialicon"
+        >
           <div className="linkedincolor socialiconcircle1" />
           <div className="socialiconcircle2 grid place-items-center">
             <FaLinkedin className="h-8 w-8 linkedin" />
           </div>
         </a>
-        <a className="socialicon">
+        <a href="mailto:rfrifat6344@gmail.com" className="socialicon">
           <div className="googlecolor socialiconcircle1"></div>
           <div className="socialiconcircle2 grid place-items-center">
             <IoMailOpenOutline className="h-8 w-8 google" />
           </div>
         </a>
-        <a className="socialicon">
+        <a
+          href="https://join.skype.com/invite/tuiuXPGhl5vt"
+          className="socialicon"
+        >
           <div className="whatsAppColor socialiconcircle1" />
           <div className="socialiconcircle2 grid place-items-center">
-            <FaSquareWhatsapp className="h-8 w-8 whatsApp" />
+            <FaSkype className="h-8 w-8 whatsApp" />
           </div>
         </a>
-        <div className="socialicon">
+        <a href="https://discord.gg/GuAuStbq" className="socialicon">
           <div className="discordColor socialiconcircle1" />
           <div className="socialiconcircle2 grid place-items-center">
             <FaDiscord className="h-8 w-8 discord" />
           </div>
-        </div>
+        </a>
       </div>
     </>
   );
